71ef9d037b8db306a2e0a573a8c7417483e2b8a2
Pramay <31489611+QuickLearner171998@users.noreply.github.com>
2020-08-29 01:36:56 +0530

    Add "997. Find the Town Judge.py"

---
+++ Graphs/997. Find the Town Judge.py
@@ -0,0 +1,37 @@
+"""
+In a town, there are N people labelled from 1 to N.  There is a rumor that one of these people is secretly the town judge.
+
+If the town judge exists, then:
+
+The town judge trusts nobody.
+Everybody (except for the town judge) trusts the town judge.
+There is exactly one person that satisfies properties 1 and 2.
+You are given trust, an array of pairs trust[i] = [a, b] representing that the person labelled a trusts the person labelled b.
+
+If the town judge exists and can be identified, return the label of the town judge.  Otherwise, return -1.
+
+
+
+Example 1:
+
+Input: N = 2, trust = [[1,2]]
+Output: 2
+
+"""
+class Solution:
+    def findJudge(self, N: int, trust: List[List[int]]) -> int:
+        indeg = [0]*(N+1)
+        outdeg = [0]*(N+1)
+        for p in trust:
+            indeg[p[1]]+=1
+
+        for p in trust:
+            outdeg[p[0]]+=1
+
+        ans = []
+        for i in range(1, N+1):
+            if indeg[i] == N-1 and outdeg[i]==0:
+                ans.append(i)
+            if len(ans) > 1:
+                return -1
+        return ans[0] if len(ans) else -1
